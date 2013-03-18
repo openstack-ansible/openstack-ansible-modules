34e27e046afc04e108679108c5a9b228a32bb639
Lorin Hochstein <lorin@nimbisservices.com>
2013-03-18 18:47:56 -0400

    Fixing broken tests

--- tests/test_keystone_service.py
+++ tests/test_keystone_service.py
@@ -13,30 +13,30 @@ def setup():
     service.name = "keystone"
     keystone.services.list = mock.Mock(return_value=[service])
     endpoints = mock.Mock(id="600759628a214eb7b3acde39b1e85180",
-                          public_url = "http://192.168.206.130:5000/v2.0",
-                          internal_url = "http://192.168.206.130:5000/v2.0",
-                          admin_url = "http://192.168.206.130:35357/v2.0")
+                          public_url="http://192.168.206.130:5000/v2.0",
+                          internal_url="http://192.168.206.130:5000/v2.0",
+                          admin_url="http://192.168.206.130:35357/v2.0")
     keystone.endpoints.list = [endpoints]
     return keystone
 
 
-@mock.patch('keystone_service.ensure_endpoints_absent')
+@mock.patch('keystone_service.ensure_endpoint_absent')
 @mock.patch('keystone_service.ensure_service_absent')
-@mock.patch('keystone_service.ensure_endpoints_present')
+@mock.patch('keystone_service.ensure_endpoint_present')
 @mock.patch('keystone_service.ensure_service_present')
 def test_dispatch_service_present(mock_ensure_service_present,
-                                  mock_ensure_endpoints_present,
+                                  mock_ensure_endpoint_present,
                                   mock_ensure_service_absent,
-                                  mock_ensure_endpoints_absent):
+                                  mock_ensure_endpoint_absent):
     """ Dispatch: service present """
     # Setup
     manager = mock.MagicMock()
     manager.attach_mock(mock_ensure_service_present, 'ensure_service_present')
     manager.attach_mock(mock_ensure_service_absent, 'ensure_service_absent')
-    manager.attach_mock(mock_ensure_endpoints_present,
-                        'ensure_endpoints_present')
-    manager.attach_mock(mock_ensure_endpoints_absent,
-                        'ensure_endpoints_absent')
+    manager.attach_mock(mock_ensure_endpoint_present,
+                        'ensure_endpoint_present')
+    manager.attach_mock(mock_ensure_endpoint_absent,
+                        'ensure_endpoint_absent')
 
     keystone = setup()
     name = "keystone"
@@ -57,32 +57,33 @@ def test_dispatch_service_present(mock_ensure_service_present,
                                                        service_type,
                                                        description,
                                                        check_mode),
-                      mock.call.ensure_endpoints_present(keystone, name,
+                      mock.call.ensure_endpoint_present(keystone, name,
                                                          public_url,
                                                          internal_url,
                                                          admin_url,
+                                                         region,
                                                          check_mode)]
 
     assert_equal(manager.mock_calls, expected_calls)
 
 
-@mock.patch('keystone_service.ensure_endpoints_absent')
+@mock.patch('keystone_service.ensure_endpoint_absent')
 @mock.patch('keystone_service.ensure_service_absent')
-@mock.patch('keystone_service.ensure_endpoints_present')
+@mock.patch('keystone_service.ensure_endpoint_present')
 @mock.patch('keystone_service.ensure_service_present')
 def test_dispatch_service_absent(mock_ensure_service_present,
-                                  mock_ensure_endpoints_present,
+                                  mock_ensure_endpoint_present,
                                   mock_ensure_service_absent,
-                                  mock_ensure_endpoints_absent):
+                                  mock_ensure_endpoint_absent):
     """ Dispatch: service absent """
     # Setup
     manager = mock.MagicMock()
     manager.attach_mock(mock_ensure_service_present, 'ensure_service_present')
     manager.attach_mock(mock_ensure_service_absent, 'ensure_service_absent')
-    manager.attach_mock(mock_ensure_endpoints_present,
-                        'ensure_endpoints_present')
-    manager.attach_mock(mock_ensure_endpoints_absent,
-                        'ensure_endpoints_absent')
+    manager.attach_mock(mock_ensure_endpoint_present,
+                        'ensure_endpoint_present')
+    manager.attach_mock(mock_ensure_endpoint_absent,
+                        'ensure_endpoint_absent')
 
     keystone = setup()
     name = "keystone"
@@ -100,7 +101,7 @@ def test_dispatch_service_absent(mock_ensure_service_present,
             public_url, internal_url, admin_url, region, state, check_mode)
 
     expected_calls = [
-        mock.call.ensure_endpoints_absent(keystone, name, check_mode),
+        mock.call.ensure_endpoint_absent(keystone, name, check_mode),
         mock.call.ensure_service_absent(keystone, name, check_mode)
     ]
 
@@ -186,7 +187,6 @@ def test_ensure_service_present_when_absent_check():
     assert not keystone.services.create.called
 
 
-
 def test_ensure_endpoint_present_when_present():
     """ ensure_endpoint_present when the endpoint is present"""
     # Setup
@@ -195,7 +195,29 @@ def test_ensure_endpoint_present_when_present():
     public_url = "http://192.168.206.130:5000/v2.0"
     internal_url = "http://192.168.206.130:5000/v2.0"
     admin_url = "http://192.168.206.130:35357/v2.0"
+    region = "RegionOne"
     check_mode = False
+
+    # Code under test
+    (changed, id) = keystone_service.ensure_endpoint_present(keystone, name,
+                        public_url, internal_url, admin_url, region,
+                        check_mode)
+
+    # Assertions
+    assert not changed
+    assert_equal(id, "b6a7ff03f2574cd9b5c7c61186e0d781")
+
+
+def test_ensure_endpoint_present_when_present_check():
+    """ ensure_endpoint_present when the endpoint is present, check mode"""
+    # Setup
+    assert False
+    keystone = setup()
+    name = "keystone"
+    public_url = "http://192.168.206.130:5000/v2.0"
+    internal_url = "http://192.168.206.130:5000/v2.0"
+    admin_url = "http://192.168.206.130:35357/v2.0"
+    check_mode = True
 
     # Code under test
     (changed, id) = keystone_service.ensure_endpoint_present(keystone, name,
