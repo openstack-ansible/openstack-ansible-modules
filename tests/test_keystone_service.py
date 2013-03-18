7f72ade23d1c6bb58abb92609d04a6088cf7c966
Lorin Hochstein <lorin@nimbisservices.com>
2013-03-18 16:36:54 -0400

    keystone_service work

---
+++ tests/test_keystone_service.py
@@ -0,0 +1,120 @@
+import keystone_service
+import mock
+from nose.tools import assert_equal, assert_is_none
+from nose.plugins.skip import SkipTest
+
+
+def setup():
+    keystone = mock.MagicMock()
+    service = mock.Mock()
+    service.id = "b6a7ff03f2574cd9b5c7c61186e0d781"
+    service.name = "keystone"
+    service.type = "identity"
+    service.description = "Keystone Identity Service"
+    keystone.services.list = mock.Mock(return_value=[service])
+    return service
+
+
+@mock.patch('keystone_service.ensure_endpoints_absent')
+@mock.patch('keystone_service.ensure_service_absent')
+@mock.patch('keystone_service.ensure_endpoints_present')
+@mock.patch('keystone_service.ensure_service_present')
+def test_dispatch_service_present(mock_ensure_service_present,
+                                  mock_ensure_endpoints_present,
+                                  mock_ensure_service_absent,
+                                  mock_ensure_endpoints_absent):
+    """ Dispatch: service present """
+    # Setup
+    manager = mock.MagicMock()
+    manager.attach_mock(mock_ensure_service_present, 'ensure_service_present')
+    manager.attach_mock(mock_ensure_service_absent, 'ensure_service_absent')
+    manager.attach_mock(mock_ensure_endpoints_present,
+                        'ensure_endpoints_present')
+    manager.attach_mock(mock_ensure_endpoints_absent,
+                        'ensure_endpoints_absent')
+
+    keystone = setup()
+    name = "keystone"
+    service_type = "identity"
+    description = "Keystone Identity Service"
+    region = "RegionOne"
+    state = "present"
+    public_url = "http://192.168.206.130:5000/v2.0"
+    internal_url = "http://192.168.206.130:5000/v2.0"
+    admin_url = "http://192.168.206.130:35357/v2.0"
+    check_mode = False
+
+    # Code under test
+    keystone_service.dispatch(keystone, name, service_type, description,
+            public_url, internal_url, admin_url, region, state, check_mode)
+
+    expected_calls = [mock.call.ensure_service_present(keystone, name,
+                                                       service_type,
+                                                       description,
+                                                       check_mode),
+                      mock.call.ensure_endpoints_present(keystone, name,
+                                                         public_url,
+                                                         internal_url,
+                                                         admin_url,
+                                                         check_mode)]
+
+    assert_equal(manager.mock_calls, expected_calls)
+
+
+@mock.patch('keystone_service.ensure_endpoints_absent')
+@mock.patch('keystone_service.ensure_service_absent')
+@mock.patch('keystone_service.ensure_endpoints_present')
+@mock.patch('keystone_service.ensure_service_present')
+def test_dispatch_service_absent(mock_ensure_service_present,
+                                  mock_ensure_endpoints_present,
+                                  mock_ensure_service_absent,
+                                  mock_ensure_endpoints_absent):
+    """ Dispatch: service absent """
+    # Setup
+    manager = mock.MagicMock()
+    manager.attach_mock(mock_ensure_service_present, 'ensure_service_present')
+    manager.attach_mock(mock_ensure_service_absent, 'ensure_service_absent')
+    manager.attach_mock(mock_ensure_endpoints_present,
+                        'ensure_endpoints_present')
+    manager.attach_mock(mock_ensure_endpoints_absent,
+                        'ensure_endpoints_absent')
+
+    keystone = setup()
+    name = "keystone"
+    service_type = "identity"
+    description = "Keystone Identity Service"
+    region = "RegionOne"
+    state = "absent"
+    public_url = "http://192.168.206.130:5000/v2.0"
+    internal_url = "http://192.168.206.130:5000/v2.0"
+    admin_url = "http://192.168.206.130:35357/v2.0"
+    check_mode = False
+
+    # Code under test
+    keystone_service.dispatch(keystone, name, service_type, description,
+            public_url, internal_url, admin_url, region, state, check_mode)
+
+    expected_calls = [
+        mock.call.ensure_endpoints_absent(keystone, name, check_mode),
+        mock.call.ensure_service_absent(keystone, name, check_mode)
+    ]
+
+    assert_equal(manager.mock_calls, expected_calls)
+
+
+def test_ensure_service_present_when_present():
+    """ ensure_services_present when the service is present"""
+    # Setup
+    keystone = setup()
+    name = "keystone"
+    service_type = "identity"
+    description = "Keystone Identity Service"
+    check_mode = False
+
+    # Code under test
+    (changed, id) = keystone_service.ensure_service_present(keystone, name,
+                        service_type, description, check_mode)
+
+    # Assertions
+    assert not changed
+    assert_equal(id, "b6a7ff03f2574cd9b5c7c61186e0d781")
